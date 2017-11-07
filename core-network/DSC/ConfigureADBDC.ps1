7ae970cab66929798b783a5c10f10c570761befc
Michael Hansen <mihansen@microsoft.com>
2017-11-07 12:14:34 -0500

    Setup for BDC added.

--- core-network/DSC/ConfigureADBDC.ps1
+++ core-network/DSC/ConfigureADBDC.ps1
@@ -1,6 +1,7 @@
 ﻿configuration ConfigureADBDC
 {
-   param
+
+	param
     (
         [Parameter(Mandatory)]
         [String]$DomainName,
@@ -12,7 +13,7 @@
         [Int]$RetryIntervalSec=30
     )
 
-    Import-DscResource -ModuleName xActiveDirectory, xPendingReboot
+    Import-DscResource -ModuleName xActiveDirectory, xStorage, xPendingReboot
 
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
 
@@ -22,7 +23,41 @@
         {
             RebootNodeIfNeeded = $true
         }
-        
+       
+        xWaitforDisk Disk2
+        {
+                DiskId = 2
+                RetryIntervalSec =$RetryIntervalSec
+                RetryCount = $RetryCount
+        }
+
+        xDisk ADDataDisk
+        {
+            DiskId = 2
+            DriveLetter = "F"
+            DependsOn = "[xWaitForDisk]Disk2"
+        }
+
+        WindowsFeature ADDSInstall
+        {
+            Ensure = "Present"
+            Name = "AD-Domain-Services"
+        }
+
+        WindowsFeature ADDSTools
+        {
+            Ensure = "Present"
+            Name = "RSAT-ADDS-Tools"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+
+        WindowsFeature ADAdminCenter
+        {
+            Ensure = "Present"
+            Name = "RSAT-AD-AdminCenter"
+            DependsOn = "[WindowsFeature]ADDSTools"
+        }
+
         xWaitForADDomain DscForestWait
         {
             DomainName = $DomainName
