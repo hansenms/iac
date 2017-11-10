8f7dbb47861000a108ce877c3fb9299e5438cd21
Michael Hansen <mihansen@microsoft.com>
2017-11-09 21:18:34 -0500

    Added SQL Server and config script for TFS

---
+++ tfs/DSC/TFSSQLServerConfig.ps1
@@ -0,0 +1,85 @@
+configuration TFSSQLServerDsc
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+		[String]$DomainNetbiosName=(Get-NetBIOSName -DomainName $DomainName),
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Admincreds,
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    )
+
+    Import-DscResource -ModuleName xComputerManagement, xActiveDirectory, xSQLServer, xSQLps
+    [System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainNetbiosName}\$($Admincreds.UserName)", $Admincreds.Password)
+
+    Node localhost
+    {
+
+        WindowsFeature ADPS
+        {
+            Name = "RSAT-AD-PowerShell"
+            Ensure = "Present"
+        }
+
+        xWaitForADDomain DscForestWait 
+        { 
+            DomainName = $DomainName 
+            DomainUserCredential= $DomainCreds
+            RetryCount = $RetryCount 
+            RetryIntervalSec = $RetryIntervalSec 
+	        DependsOn = "[WindowsFeature]ADPS"
+        }
+        
+        xComputer DomainJoin
+        {
+            Name = $env:COMPUTERNAME
+            DomainName = $DomainName
+            Credential = $DomainCreds
+	        DependsOn = "[xWaitForADDomain]DscForestWait"
+        }
+
+        xSqlServerLogin AddDomainAdminAccountToSysadminServerRole
+        {
+            Name = $DomainCreds.UserName
+            LoginType = "WindowsUser"
+            LoginCredential = $Admincreds
+			SQLServer = $end:COMPUTERNAME
+			SQLInstanceName = $env:COMPUTERNAME
+        }
+		
+        LocalConfigurationManager 
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+    }
+}
+
+function Get-NetBIOSName
+{ 
+    [OutputType([string])]
+    param(
+        [string]$DomainName
+    )
+
+    if ($DomainName.Contains('.')) {
+        $length=$DomainName.IndexOf('.')
+        if ( $length -ge 16) {
+            $length=15
+        }
+        return $DomainName.Substring(0,$length)
+    }
+    else {
+        if ($DomainName.Length -gt 15) {
+            return $DomainName.Substring(0,15)
+        }
+        else {
+            return $DomainName
+        }
+    }
+}
