499b4d4f860dda51b9e2335bf1dd7f21c4d82986
Michael Hansen <mihansen@microsoft.com>
2017-12-17 13:32:16 -0500

    Added clustering to SQL VMs

--- sql-alwayson/DSC/PrepareSQLServer.ps1
+++ sql-alwayson/DSC/PrepareSQLServer.ps1
@@ -72,6 +72,24 @@ configuration SQLServerPrepareDsc
             Ensure = "Present"
         }
 
+        WindowsFeature FC
+        {
+            Name = "Failover-Clustering"
+            Ensure = "Present"
+        }
+
+        WindowsFeature FailoverClusterTools 
+        { 
+            Ensure = "Present" 
+            Name = "RSAT-Clustering-Mgmt"
+            DependsOn = "[WindowsFeature]FC"
+        } 
+
+        WindowsFeature FCPS
+        {
+            Name = "RSAT-Clustering-PowerShell"
+            Ensure = "Present"
+        }
 
         WindowsFeature ADPS
         {
---
+++ sql-alwayson/README.md
@@ -0,0 +1,7 @@
+
+
+To Do List
+-----------
+
+* Create service user (domain user) for running SQL services
+* Move SQL server folders to F: drive. Currently failing.
