ff05f23b4e8b00eb7c606ce404eac43855df232d
Michael Hansen <mihansen@microsoft.com>
2018-01-09 11:53:03 -0500

    Updated IaaS Web to include dotnetcore

--- iaas-web/DSC/ConfigureWeb.ps1
+++ iaas-web/DSC/ConfigureWeb.ps1
@@ -15,13 +15,52 @@ configuration ConfigureWebDsc
             Name            = 'Web-Server'
         }
 
+        WindowsFeature aspnet45
+        {
+            Ensure          = 'Present'
+            Name            = 'Web-Asp-Net45'
+            DependsOn       = '[WindowsFeature]IIS'
+        }
+        
+        WindowsFeature NetFrameworkFeature
+        {
+            Ensure          = 'Present'
+            Name            = 'NET-Framework-Features'
+            DependsOn       = '[WindowsFeature]aspnet45'
+        }
+
         xWebsite DefaultSite 
         {
             Ensure          = 'Present'
             Name            = 'Default Web Site'
             State           = 'Started'
             PhysicalPath    = 'C:\inetpub\wwwroot'
-            DependsOn       = '[WindowsFeature]IIS'
+            DependsOn       = '[WindowsFeature]NetFrameworkFeature'
+        }
+
+        Script InstallDotNetStuff
+        {
+            GetScript = { 
+                return @{ 'Result' = $true }
+            }
+
+            TestScript = { 
+                Test-Path $env:temp\dotnet-dev-win-x64.1.0.4.exe
+            }
+            SetScript = {
+                # Install the .NET Core SDK
+                Invoke-WebRequest https://go.microsoft.com/fwlink/?linkid=848827 -outfile $env:temp\dotnet-dev-win-x64.1.0.4.exe
+                Start-Process $env:temp\dotnet-dev-win-x64.1.0.4.exe -ArgumentList '/quiet' -Wait
+
+                # Install the .NET Core Windows Server Hosting bundle
+                Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkId=817246 -outfile $env:temp\DotNetCore.WindowsHosting.exe
+                Start-Process $env:temp\DotNetCore.WindowsHosting.exe -ArgumentList '/quiet' -Wait
+
+                # Restart the web server so that system PATH updates take effect
+                net stop was /y
+                net start w3svc
+            }
+            DependsOn = "[xWebsite]DefaultSite"
         }
 
         File WebContent
@@ -31,7 +70,7 @@ configuration ConfigureWebDsc
             Recurse         = $true
             Type            = 'File'
             Contents        = '<html><body><h1>This is the default website</h1></body></html>' 
-            DependsOn       = '[WindowsFeature]IIS'
+            DependsOn       = '[xWebsite]DefaultSite'
         }       
     }
 }
