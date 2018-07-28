9a58f636fec6bfea1f30f865c4f4e72c4adc77e7
Michael Hansen <mihansen@microsoft.com>
2018-07-28 08:39:10 -0400

    Orchestrating from setupscript

--- gadgetron-workstation/scripts/setup_gadgetron_workstation.sh
+++ gadgetron-workstation/scripts/setup_gadgetron_workstation.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+USERNAME=$1
+CURRENT_FOLDER=${PWD}
+BOOTSTRAP_SCRIPT="bootstrap_gadgetron.sh"
+
 apt-get update
 
 apt-get install --yes software-properties-common apt-utils wget build-essential cython emacs python-dev python-pip python3-dev python3-pip libhdf5-serial-dev cmake git-core libboost-all-dev libfftw3-dev h5utils jq hdf5-tools liblapack-dev libopenblas-base libopenblas-dev libxml2-dev libfreetype6-dev pkg-config libxslt-dev libarmadillo-dev libace-dev gcc-multilib libgtest-dev python3-dev liblapack-dev liblapacke-dev libplplot-dev libdcmtk-dev cmake-curses-gui cmake neofetch net-tools cpio x2goserver ubuntu-mate-core emacs
@@ -44,3 +48,8 @@ cd ceres-bin && \
 cmake ../ceres-solver-1.14.0 && \
 make -j $(nproc) && \
 make install
+
+cp ${CURRENT_FOLDER}/${BOOTSTRAP_SCRIPT} /home/${USERNAME}/
+chown ${USERNAME}:${USERNAME} /home/${USERNAME}/${BOOTSTRAP_SCRIPT}
+chmod +x /home/${USERNAME}/${BOOTSTRAP_SCRIPT}
+su -c /home/${USERNAME}/${BOOTSTRAP_SCRIPT} ${USERNAME}
